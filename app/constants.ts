af0785a79b8e9987a9c0643a5aeaefaa192d92be
James Ritchie <james@jamesritchie.co.uk>
2023-11-20 17:52:04 +0000

    Added some new trash button options

--- app/constants.ts
+++ app/constants.ts
@@ -1,10 +1,19 @@
 export const voices = {
   pirate: { label: "🏴‍☠️ Pirate", value: "a pirate" },
-  shakespeare: { label: "🎭 Shakespeare", value: "William Shakespeare" },
-  yoda: { label: "🧙 Yoda", value: "Master Yoda" },
   cuss: {
     label: "🤬 F*ck it!",
     value: "cuss words (use a*terisks to make it safe)",
   },
   boomer: { label: "👴 Boomer", value: "tech-illiterate boomer" },
+  // aliG: { label: "Ali G", value: "in the style of Ali G" },
+  // valley: { label: "Valley Girl", value: "in the style of a Valley Girl" },
+  surfer: {
+    label: "🏄 Surfer",
+    value: "in the style of a Surfer Dude like Bill and Ted",
+  },
+  terminator: {
+    label: "💀 The Terminator",
+    value: "in the style of Arnold Schwarzenegger's Terminator",
+  },
+  // british: { label: "British", value: "Overly Formal British English" },
 };
